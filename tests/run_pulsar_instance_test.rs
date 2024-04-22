215b49ed125973d1d8e686c4d4c7f72a04d03add
visortelle <visortelle@gmail.com>
2024-04-23 02:26:14 +0400

    Improve tests

---
+++ tests/run_pulsar_instance_test.rs
@@ -0,0 +1,235 @@
+mod utils;
+
+use anyhow::Result;
+use assert_cmd::cargo::CommandCargoExt;
+use pulsar_compose::instance_config::{self, InstanceConfig};
+use std::process::Command;
+use tokio::time::{sleep, Duration};
+use utils::{
+    check_cluster_connection, check_cluster_exists, check_namespace_exists, check_tenant_exists,
+    kill_all_docker_containers, rand_instance_name,
+};
+
+async fn test_pulsar_instance(instance_config: InstanceConfig) -> Result<()> {
+    let instance_config_yaml = serde_yaml::to_string(&instance_config)?;
+    println!("Testing instance: {instance_config_yaml}");
+
+    let instance_config_clone = instance_config.clone();
+    tokio::spawn(async move {
+        let instance_name = instance_config_clone.name.clone();
+        let num_clusters = instance_config_clone.num_clusters;
+        let num_zookeepers = instance_config_clone.num_zookeepers;
+        let num_bookies = instance_config_clone.num_bookies;
+        let num_brokers = instance_config_clone.num_brokers;
+
+        println!("Creating instance: {instance_name}");
+        let exit_status = Command::cargo_bin("pulsar-compose")
+            .unwrap()
+            .arg("create")
+            .arg("--name")
+            .arg(instance_name.clone())
+            .arg("--num-clusters")
+            .arg(num_clusters.to_string())
+            .arg("--num-zookeepers")
+            .arg(num_zookeepers.to_string())
+            .arg("--num-bookies")
+            .arg(num_bookies.to_string())
+            .arg("--num-brokers")
+            .arg(num_brokers.to_string())
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap();
+
+        assert!(exit_status.success());
+
+        println!("Running instance: {instance_name}");
+        Command::cargo_bin("pulsar-compose")
+            .unwrap()
+            .arg("run")
+            .arg(instance_name.clone())
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap()
+    });
+
+    let is_able_to_connect_to_all_clusters = tokio::spawn(async move {
+        let started_at = std::time::Instant::now();
+        loop {
+            let futures = (0..instance_config.num_clusters)
+                .map(check_cluster_connection)
+                .collect::<Vec<_>>();
+            let results = futures::future::join_all(futures).await;
+
+            match results.iter().all(|r| r.is_ok()) {
+                true => return true,
+                false => {
+                    let time_elapsed = started_at.elapsed();
+                    sleep(Duration::from_secs(10)).await;
+
+                    if time_elapsed.as_secs() > 60 * 3 {
+                        return false;
+                    }
+                }
+            }
+        }
+    })
+    .await?;
+
+    assert!(is_able_to_connect_to_all_clusters);
+
+    let are_clusters_created = tokio::spawn(async move {
+        let started_at = std::time::Instant::now();
+        loop {
+            let futures = (0..instance_config.num_clusters)
+                .map(|i| check_cluster_exists(i, format!("cluster-{i}")))
+                .collect::<Vec<_>>();
+            let results = futures::future::join_all(futures).await;
+
+            match results.iter().all(|r| r.is_ok()) {
+                true => return true,
+                false => {
+                    let time_elapsed = started_at.elapsed();
+                    sleep(Duration::from_secs(10)).await;
+
+                    if time_elapsed.as_secs() > 60 * 3 {
+                        return false;
+                    }
+                }
+            }
+        }
+    })
+    .await?;
+
+    assert!(are_clusters_created);
+
+    let are_tenants_created = tokio::spawn(async move {
+        let started_at = std::time::Instant::now();
+        loop {
+            let futures = (0..instance_config.num_clusters)
+                .map(|i| check_tenant_exists(i, format!("cluster-{i}-local")))
+                .collect::<Vec<_>>();
+            let results = futures::future::join_all(futures).await;
+
+            match results.iter().all(|r| r.is_ok()) {
+                true => return true,
+                false => {
+                    let time_elapsed = started_at.elapsed();
+                    sleep(Duration::from_secs(10)).await;
+
+                    if time_elapsed.as_secs() > 60 * 3 {
+                        return false;
+                    }
+                }
+            }
+        }
+    })
+    .await?;
+
+    assert!(are_tenants_created);
+
+    let is_namespace_exists = tokio::spawn(async move {
+        let started_at = std::time::Instant::now();
+        loop {
+            let futures = (0..instance_config.num_clusters)
+                .map(|i| {
+                    check_namespace_exists(
+                        i,
+                        format!("cluster-{i}-local"),
+                        "default".to_string(),
+                    )
+                })
+                .collect::<Vec<_>>();
+
+            let results = futures::future::join_all(futures).await;
+
+            match results.iter().all(|r| r.is_ok()) {
+                true => return true,
+                false => {
+                    let time_elapsed = started_at.elapsed();
+                    sleep(Duration::from_secs(10)).await;
+
+                    if time_elapsed.as_secs() > 60 * 10 {
+                        return false;
+                    }
+                }
+            }
+        }
+    })
+    .await?;
+
+    assert!(is_namespace_exists);
+
+    kill_all_docker_containers()
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_run_pulsar_instance_c1_br1_bo1_zo1() {
+    test_pulsar_instance(InstanceConfig {
+        name: rand_instance_name(),
+        pulsar_version: "3.2.2".to_string(),
+        num_clusters: 1,
+        num_brokers: 1,
+        num_bookies: 1,
+        num_zookeepers: 1,
+    })
+    .await
+    .unwrap();
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_run_pulsar_instance_c2_br1_bo1_zo1() {
+    test_pulsar_instance(InstanceConfig {
+        name: rand_instance_name(),
+        pulsar_version: "3.2.2".to_string(),
+        num_clusters: 2,
+        num_brokers: 1,
+        num_bookies: 1,
+        num_zookeepers: 1,
+    })
+    .await
+    .unwrap();
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_run_pulsar_instance_c3_br1_bo1_zo1() {
+    test_pulsar_instance(InstanceConfig {
+        name: rand_instance_name(),
+        pulsar_version: "3.2.2".to_string(),
+        num_clusters: 3,
+        num_brokers: 1,
+        num_bookies: 1,
+        num_zookeepers: 1,
+    })
+    .await
+    .unwrap();
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_run_pulsar_instance_c1_br3_bo3_zo3() {
+    test_pulsar_instance(InstanceConfig {
+        name: rand_instance_name(),
+        pulsar_version: "3.2.2".to_string(),
+        num_clusters: 1,
+        num_brokers: 3,
+        num_bookies: 3,
+        num_zookeepers: 3,
+    })
+    .await
+    .unwrap();
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_run_pulsar_instance_c1_br2_bo2_zo1() {
+    test_pulsar_instance(InstanceConfig {
+        name: rand_instance_name(),
+        pulsar_version: "3.2.2".to_string(),
+        num_clusters: 1,
+        num_brokers: 2,
+        num_bookies: 2,
+        num_zookeepers: 1,
+    })
+    .await
+    .unwrap();
+}
--- tests/utils.rs
+++ tests/utils.rs
@@ -1,34 +1,38 @@
 use anyhow::Result;
+use std::process::{Command, Stdio};
 use uuid::Uuid;
-use assert_cmd::Command;
 
 pub fn rand_instance_name() -> String {
     "pulsar-".to_string() + &Uuid::new_v4().to_string()
 }
 
-pub fn kill_all_docker_containers() {
+pub fn kill_all_docker_containers() -> Result<()> {
     println!("Killing all docker containers");
 
     Command::new("bash")
+        .stderr(Stdio::piped())
         .arg("-c")
         .arg("docker ps -q | xargs docker kill")
-        .assert()
-        .success();
+        .spawn()
+        .map_err(|e| anyhow::anyhow!("Failed to kill all docker containers: {e}"))?;
 
     print!("All docker containers successfully killed");
+
+    Ok(())
 }
 
 pub async fn check_cluster_exists(instance_index: u32, cluster_name: String) -> Result<()> {
     let port: u32 = format!("{instance_index}8080").parse().unwrap();
     let out = Command::new("pulsar-admin")
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
         .arg("--admin-url")
         .arg(format!("http://localhost:{port}"))
         .arg("clusters")
         .arg("list")
-        .assert()
-        .get_output()
-        .stdout
-        .clone();
+        .spawn()?
+        .wait_with_output()?
+        .stdout;
 
     let is_exists = String::from_utf8(out)?
         .lines()
@@ -45,14 +49,15 @@ pub async fn check_cluster_exists(instance_index: u32, cluster_name: String) -> 
 pub async fn check_tenant_exists(cluster_index: u32, tenant: String) -> Result<()> {
     let port: u32 = format!("{cluster_index}8080").parse().unwrap();
     let out = Command::new("pulsar-admin")
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
         .arg("--admin-url")
         .arg(format!("http://localhost:{port}"))
         .arg("tenants")
         .arg("list")
-        .assert()
-        .get_output()
-        .stdout
-        .clone();
+        .spawn()?
+        .wait_with_output()?
+        .stdout;
 
     let is_exists = String::from_utf8(out)?
         .lines()
@@ -62,22 +67,27 @@ pub async fn check_tenant_exists(cluster_index: u32, tenant: String) -> Result<(
 
     match is_exists {
         true => Ok(()),
-        false => Err(anyhow::anyhow!("Cluster not exists")),
+        false => Err(anyhow::anyhow!("Tenant not exists")),
     }
 }
 
-pub async fn check_namespace_exists(cluster_index: u32, tenant: String, namespace: String) -> Result<()> {
+pub async fn check_namespace_exists(
+    cluster_index: u32,
+    tenant: String,
+    namespace: String,
+) -> Result<()> {
     let port: u32 = format!("{cluster_index}8080").parse().unwrap();
     let out = Command::new("pulsar-admin")
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
         .arg("--admin-url")
         .arg(format!("http://localhost:{port}"))
         .arg("namespaces")
         .arg("list")
         .arg(tenant)
-        .assert()
-        .get_output()
-        .stdout
-        .clone();
+        .spawn()?
+        .wait_with_output()?
+        .stdout;
 
     let is_exists = String::from_utf8(out)?
         .lines()
@@ -87,7 +97,28 @@ pub async fn check_namespace_exists(cluster_index: u32, tenant: String, namespac
 
     match is_exists {
         true => Ok(()),
-        false => Err(anyhow::anyhow!("Cluster not exists")),
+        false => Err(anyhow::anyhow!("Namespace not exists")),
+    }
+}
+
+pub async fn check_cluster_connection(
+    cluster_index: u32,
+) -> Result<()> {
+    let port: u32 = format!("{cluster_index}8080").parse().unwrap();
+    let is_ok = Command::new("pulsar-admin")
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .arg("--admin-url")
+        .arg(format!("http://localhost:{port}"))
+        .arg("brokers")
+        .arg("healthcheck")
+        .spawn()?.wait()?.success();
+
+    println!("Is able to connect to cluster {cluster_index}: {is_ok}");
+
+    match is_ok {
+        true => Ok(()),
+        false => Err(anyhow::anyhow!("Unable to connect to cluster {cluster_index}")),
     }
 }
 
